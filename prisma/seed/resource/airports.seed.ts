07b7654bb79f98d6023297c6f158fa8d520d4284
Oskar Barcz <ob@gi.org.pl>
2025-01-25 12:41:33 +0100

    Prepare database for new city and iata code fields

--- prisma/seed/resource/airports.seed.ts
+++ prisma/seed/resource/airports.seed.ts
@@ -4,6 +4,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const frankfurt: Airport = {
     id: 'f35c094a-bec5-4803-be32-bd80a14b441a',
     icaoCode: 'EDDF',
+    iataCode: 'FRA',
+    city: 'Frankfurt',
     name: 'Frankfurt Rhein/Main',
     country: 'Germany',
     timezone: 'Europe/Berlin',
@@ -12,6 +14,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const warsaw: Airport = {
     id: '616cbdd7-ccfc-4687-8cf6-1e7236435046',
     icaoCode: 'EPWA',
+    iataCode: 'WAW',
+    city: 'Warsaw',
     name: 'Warsaw Chopin',
     country: 'Poland',
     timezone: 'Europe/Warsaw',
@@ -20,6 +24,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const newYork: Airport = {
     id: '3c721cc6-c653-4fad-be43-dc9d6a149383',
     icaoCode: 'KJFK',
+    iataCode: 'JFK',
+    city: 'New York',
     name: 'New York JFK',
     country: 'United States of America',
     timezone: 'America/New_York',
@@ -28,6 +34,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const paris: Airport = {
     id: '79b8f884-f67d-4585-b540-36b0be7f551e',
     icaoCode: 'LFPG',
+    iataCode: 'CDG',
+    city: 'Paris',
     name: 'Paris Charles de Gaulle',
     country: 'France',
     timezone: 'Europe/Paris',
@@ -36,6 +44,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const gooseBay: Airport = {
     id: 'fa8ee2e9-fb94-4416-9ed0-4811efd488ae',
     icaoCode: 'CYYR',
+    iataCode: 'YYR',
+    city: 'Goose Bay',
     name: 'Goose Bay Intl',
     country: 'Canada',
     timezone: 'America/Goose_Bay',
@@ -44,6 +54,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const reykjavik: Airport = {
     id: '523b2d2f-9b60-405a-bd5a-90eed1b58e9a',
     icaoCode: 'BIRK',
+    iataCode: 'KEF',
+    city: 'Reykjavik',
     name: 'Reykjavik Keflavik',
     country: 'Iceland',
     timezone: 'GMT',
@@ -52,6 +64,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const stJohns: Airport = {
     id: '6cf1fcd8-d072-46b5-8132-bd885b43dd97',
     icaoCode: 'CYYT',
+    iataCode: 'YYT',
+    city: 'St. Johns',
     name: 'St. Johns Intl',
     country: 'Canada',
     timezone: 'GMT-3:30',
@@ -60,6 +74,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const philadelphia: Airport = {
     id: 'e764251b-bb25-4e8b-8cc7-11b0397b4554',
     icaoCode: 'KPHL',
+    iataCode: 'PHL',
+    city: 'Philadelphia',
     name: 'Philadelphia Intl',
     country: 'United States of America',
     timezone: 'GMT-5',
@@ -68,6 +84,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const boston: Airport = {
     id: 'c03a79fb-c5ae-46c3-95fe-f3b5dc7b85f3',
     icaoCode: 'KBOS',
+    iataCode: 'BOS',
+    city: 'Boston',
     name: 'Boston Logan Intl',
     country: 'United States of America',
     timezone: 'GMT-5',
@@ -76,6 +94,8 @@ export async function loadAirports(prisma: PrismaClient): Promise<void> {
   const bremen: Airport = {
     id: '5c88ea21-f482-47ff-8b1f-3d0c9bbd6caf',
     icaoCode: 'EDDW',
+    iataCode: 'BRE',
+    city: 'Bremen',
     name: 'Bremen',
     country: 'Germany',
     timezone: 'GMT+1',
