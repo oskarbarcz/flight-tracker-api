ea421760ea442a416527e16be6b8a29cf52d6de2
Oskar Barcz <ob@gi.org.pl>
2025-02-09 02:14:08 +0100

    Bump version for release

--- src/flights/entities/loadsheet.entity.ts
+++ src/flights/entities/loadsheet.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class FlightCrew {
