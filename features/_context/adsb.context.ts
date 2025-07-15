62817dfd48a4331036b227e094c500ba1ef1e4e8
Oskar Barcz <ob@gi.org.pl>
2025-07-15 20:20:14 +0200

    Bump version for release

--- features/_context/adsb.context.ts
+++ features/_context/adsb.context.ts
@@ -3,9 +3,11 @@ import { deepCompare } from '../_helper/deep-compare';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 Then(
   'ADSB service was requested for callsign {string} and returned data:',
-  (callsign: string, body: string) => {
+  async (callsign: string, body: string) => {
     const expected = JSON.parse(body);
     const filePath = path.join(
       process.cwd(),
@@ -13,6 +15,7 @@ Then(
       'adsb',
       `${callsign}.json`,
     );
+    await delay(1000);
     const actual = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
     deepCompare(actual, expected);
