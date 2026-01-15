b5241b0c1ab13c705f8b98a4161c401a41ee9ae2
Oskar Barcz <oskar@barcz.me>
2026-01-15 18:17:31 +0100

    Remove skip generate from database context

--- features/_context/database.context.ts
+++ features/_context/database.context.ts
@@ -3,20 +3,20 @@ import { execSync } from 'child_process';
 
 // Reset the database before running tests
 BeforeAll(() => {
-  execSync('npx prisma migrate reset --force --skip-generate', {
+  execSync('npx prisma migrate reset --force', {
     stdio: 'ignore',
   });
 });
 
 // Reset the database after running tests
 AfterAll(() => {
-  execSync('npx prisma migrate reset --force --skip-generate', {
+  execSync('npx prisma migrate reset --force', {
     stdio: 'ignore',
   });
 });
 
 Then('I set database to initial state', () => {
-  execSync('npx prisma migrate reset --force --skip-generate', {
+  execSync('npx prisma migrate reset --force', {
     stdio: 'ignore',
   });
 });
