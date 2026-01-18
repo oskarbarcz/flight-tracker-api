635e8482d9d7e165dd87c549ce367f24db90edc7
Oskar Barcz <oskar@barcz.me>
2026-01-18 22:52:21 +0100

    Revert "Update src/modules/flights/application/query/check-flight-exists.query.ts"
    This reverts commit 5bf922d419ea64d92afaefe2bbebdfcbc4185782.

--- src/modules/flights/application/query/check-flight-exists.query.ts
+++ src/modules/flights/application/query/check-flight-exists.query.ts
@@ -12,7 +12,7 @@ export class CheckFlightExistsHandler implements IQueryHandler<CheckFlightExists
   constructor(private readonly repository: FlightsRepository) {}
 
   async execute(query: CheckFlightExistsQuery): Promise<boolean> {
-    const count = await this.repository.countBy({ id: query.flightId });
-    return count > 0;
+    const flight = await this.repository.findOneBy({ id: query.flightId });
+    return !!flight;
   }
 }
